ded943d32af89c425e1c696e6bb352f42d1ee9c3
belaifa_el_hussein <belaifahocine@gmail.com>
2019-06-05 14:05:34 +0200

    modify navbar opacity when scrollling with jquery code

--- js/main.js
+++ js/main.js
@@ -1,9 +1,9 @@
-window.addEventListener('scroll', function () {
- if (window.scrollY > 150) {
-  document.querySelector('#navbar').style.opacity = 0.9;
- } else document.querySelector('#navbar').style.opacity = 1;
+$(window).on('scroll', function () {
+ console.log('scrolling');
+ console.log($(this).scrollTop());
+ if ($(this).scrollTop() > 150) $('#navbar').css('opacity', 0.9);
+ else $('#navbar').css('opacity', 1);
 })
-
 
 
 $('#navbar a, .btn').on('click', function (event) {
